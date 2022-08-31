47e98a926376b9faccaacc77b0a2da4a3e641da5
loc-giang <locbgiang@gmail.com>
2022-08-31 15:01:26 -0700

    fixed route

--- client/src/components/Router.jsx
+++ client/src/components/Router.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Routes, Route } from 'react-router-dom';
-import Result from "./Result";
+import {Result} from "./Result";
 
 export const Router = () => {
     return(
         <div className="p-4">
-             <Routes>
+            hi
+            <Routes>
                 <Route path='/' element={<Result />}/>
             </Routes>
         </div>
