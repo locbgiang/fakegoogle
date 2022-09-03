a3d8bb0883083394ee03543cf65c22740010373e
loc-giang <locbgiang@gmail.com>
2022-09-02 23:34:45 -0700

    routes set up

--- client/src/components/Router.jsx
+++ client/src/components/Router.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import {Result} from "./Result";
 
 export const Router = () => {
     return(
         <div className="p-4">
-            hi
             <Routes>
-                <Route path='/' element={<Result />}/>
+                <Route path='/' element={<Navigate from='/' to='/search' />}/>
+                <Route path='/search' element={<Result />} />
+                <Route path='/images' element={<Result />} />
+                <Route path='/news' element={<Result />} />
+                <Route path='/videos' element={<Result />} />
             </Routes>
         </div>
     )
